b84a0f11b5748af7825239025207624fdc125d5b
Ennio Simoes <ennio.simoes@outlook.com>
2016-08-14 00:01:56 -0300

    first commit

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import App from './App.vue'
+import BlockContent from './BlockContent.vue'
+
+new Vue({
+  el: 'body',
+  components: {
+      App,
+      BlockContent
+  }
+})
